3e6f03db5d54d83b4a4fc86212ae9735d3a49641
no-chili <18875854140@163.com>
2023-01-04 17:58:59 +0800

    l

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,23 +25,7 @@ packer.startup({function(use)
       requires = { {'nvim-lua/plenary.nvim'} }
   }
   use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-   --------------------- LSP --------------------
-  use("williamboman/nvim-lsp-installer")
-   -- Lspconfig
- -- 补全引擎
-        use("hrsh7th/nvim-cmp")
-        -- snippet 引擎
-        use("hrsh7th/vim-vsnip")
-        -- 补全源
-        use("hrsh7th/cmp-vsnip")
-        use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
-        use("hrsh7th/cmp-buffer") -- { name = 'buffer' },
-        use("hrsh7th/cmp-path") -- { name = 'path' }
-        use("hrsh7th/cmp-cmdline") -- { name = 'cmdline' }
-
-        -- 常见编程语言代码段
-        use("rafamadriz/friendly-snippets") use({ "neovim/nvim-lspconfig" })
-end,
+ end,
 config = {
     -- 并发数限制
     max_jobs = 16,
